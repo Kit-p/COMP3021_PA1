ccbda2797a3653731462a2b53ac928903701b63b
Kit-p <kpangaa@connect.ust.hk>
2020-09-27 20:38:48 +0800

    add Knight validateMove()

--- src/main/java/castle/comp3021/assignment/piece/Knight.java
+++ src/main/java/castle/comp3021/assignment/piece/Knight.java
@@ -1,10 +1,6 @@
 package castle.comp3021.assignment.piece;
 
-import castle.comp3021.assignment.protocol.Game;
-import castle.comp3021.assignment.protocol.Move;
-import castle.comp3021.assignment.protocol.Piece;
-import castle.comp3021.assignment.protocol.Place;
-import castle.comp3021.assignment.protocol.Player;
+import castle.comp3021.assignment.protocol.*;
 
 /**
  * Knight piece that moves similar to knight in chess.
@@ -41,5 +37,69 @@ public class Knight extends Piece {
     public Move[] getAvailableMoves(Game game, Place source) {
         // TODO student implementation
         return new Move[0];
+    }
+
+    /**
+     * Validate a move
+     * This is a utility method private to this class.
+     * This method must be called in this.getAvailableMoves().
+     * It will check boundaries of the gameboard, move protection and the rules specific to Knight.
+     *
+     * @param game the game object
+     * @param move the move to be validated
+     * @return a boolean to indicate the validity of the move
+     */
+    private boolean validateMove(Game game, Move move) {
+        if (game == null || move == null) {
+            return false;
+        }
+
+        Configuration configuration = game.getConfiguration();
+        int numMoves = game.getNumMoves();
+        Place source = move.getSource();
+        int sourceX = source.x();
+        int sourceY = source.y();
+        Place destination = move.getDestination();
+        int destinationX = destination.x();
+        int destinationY = destination.y();
+        int distanceX = Math.abs(destinationX - sourceX);
+        int distanceY = Math.abs(destinationY - sourceY);
+
+        Piece capturedPiece = game.getPiece(destinationX, destinationY);
+        if (capturedPiece != null && (numMoves < configuration.getNumMovesProtection()
+                || capturedPiece.getPlayer().equals(this.getPlayer()))) {
+            return false;
+        }
+
+        // validate coordinates
+        int size = configuration.getSize();
+        if (destinationX >= size || destinationY >= size
+                || sourceX >= size || sourceY >= size
+                || destinationX < 0 || destinationY < 0
+                || sourceX < 0 || sourceY < 0) {
+            return false;
+        }
+        if (distanceX == 0 && distanceY == 0) {
+            return false;
+        }
+
+        // validate move
+        boolean isMoveHorizontal = (distanceX == 2 && distanceY == 1);
+        boolean isMoveVertical = (distanceX == 1 && distanceY == 2);
+        int blockingX, blockingY;
+        if (isMoveHorizontal) {
+            blockingX = (sourceX + destinationX) / 2;
+            blockingY = sourceY;
+        } else if (isMoveVertical) {
+            blockingX = sourceX;
+            blockingY = (sourceY + destinationY) / 2;
+        } else {
+            return false;
+        }
+        if (game.getPiece(blockingX, blockingY) != null) {
+            return false;
+        }
+
+        return true;
     }
 }
