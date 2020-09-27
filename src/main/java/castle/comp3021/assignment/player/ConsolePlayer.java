eb19290ee991ddab9529962cbc594af0b55e09c8
Kit-p <kpangaa@connect.ust.hk>
2020-09-27 21:00:14 +0800

    add ConsolePlayer validateMove()

--- src/main/java/castle/comp3021/assignment/player/ConsolePlayer.java
+++ src/main/java/castle/comp3021/assignment/player/ConsolePlayer.java
@@ -1,5 +1,7 @@
 package castle.comp3021.assignment.player;
 
+import castle.comp3021.assignment.piece.Archer;
+import castle.comp3021.assignment.piece.Knight;
 import castle.comp3021.assignment.protocol.*;
 import org.jetbrains.annotations.NotNull;
 
@@ -42,5 +44,123 @@ public class ConsolePlayer extends Player {
     public @NotNull Move nextMove(Game game, Move[] availableMoves) {
         // TODO student implementation
         return availableMoves[0];
+    }
+
+    /**
+     * Validate a move
+     * This is a utility method private to this class.
+     * This method must be called before executing the move.
+     * It will check the boundaries of the gameboard, move protection, the rules specific to the piece, and the invoking player.
+     *
+     * @param game   the game object
+     * @param move   the move to be validated
+     * @return a boolean to indicate the validity of the move
+     */
+    private boolean validateMove(Game game, Move move) {
+        if (game == null || move == null) {
+            return false;
+        }
+
+        Configuration configuration = game.getConfiguration();
+        int numMoves = game.getNumMoves();
+        Place source = move.getSource();
+        int sourceX = source.x();
+        int sourceY = source.y();
+        Place destination = move.getDestination();
+        int destinationX = destination.x();
+        int destinationY = destination.y();
+        int distanceX = Math.abs(destinationX - sourceX);
+        int distanceY = Math.abs(destinationY - sourceY);
+        Piece piece = game.getPiece(sourceX, sourceY);
+
+        if (!piece.getPlayer().equals(this)) {
+            return false;
+        }
+
+        Piece capturedPiece = game.getPiece(destinationX, destinationY);
+        if (capturedPiece != null && (numMoves < configuration.getNumMovesProtection()
+                || capturedPiece.getPlayer().equals(this))) {
+            return false;
+        }
+
+        // validate coordinates
+        int size = configuration.getSize();
+        if (destinationX >= size || destinationY >= size
+                || sourceX >= size || sourceY >= size
+                || destinationX < 0 || destinationY < 0
+                || sourceX < 0 || sourceY < 0) {
+            return false;
+        }
+        if (distanceX == 0 && distanceY == 0) {
+            return false;
+        }
+
+        // validate move
+        if (piece instanceof Knight) {
+            boolean isMoveHorizontal = (distanceX == 2 && distanceY == 1);
+            boolean isMoveVertical = (distanceX == 1 && distanceY == 2);
+            int blockingX, blockingY;
+            if (isMoveHorizontal) {
+                blockingX = (sourceX + destinationX) / 2;
+                blockingY = sourceY;
+            } else if (isMoveVertical) {
+                blockingX = sourceX;
+                blockingY = (sourceY + destinationY) / 2;
+            } else {
+                return false;
+            }
+            if (game.getPiece(blockingX, blockingY) != null) {
+                return false;
+            }
+        } else if (piece instanceof Archer) {
+            boolean isMoveHorizontal = (distanceY == 0);
+            boolean isMoveVertical = (distanceX == 0);
+            int start, end;
+            int inBetweenPieceCount = 0;
+            if (isMoveHorizontal) {
+                if (sourceX < destinationX) {
+                    start = sourceX;
+                    end = destinationX;
+                } else {
+                    start = destinationX;
+                    end = sourceX;
+                }
+                for (int x = start; x < end; x++) {
+                    if (game.getPiece(x, sourceY) != null) {
+                        inBetweenPieceCount++;
+                    }
+                    if (inBetweenPieceCount > 1) {
+                        break;
+                    }
+                }
+            } else if (isMoveVertical) {
+                if (sourceY < destinationY) {
+                    start = sourceY;
+                    end = destinationY;
+                } else {
+                    start = destinationY;
+                    end = sourceY;
+                }
+                for (int y = start; y < end; y++) {
+                    if (game.getPiece(sourceX, y) != null) {
+                        inBetweenPieceCount++;
+                    }
+                    if (inBetweenPieceCount > 1) {
+                        break;
+                    }
+                }
+            } else {
+                return false;
+            }
+            if (capturedPiece != null && inBetweenPieceCount != 1) {
+                return false;
+            } else if (capturedPiece == null && inBetweenPieceCount != 0) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        return true;
     }
 }
