e44390cc53c5935dfef3a79d966ad04b5d37a28a
Kit-p <kpangaa@connect.ust.hk>
2020-10-04 01:00:35 +0800

    extend AdditionalTests

--- src/test/java/castle/comp3021/assignment/AdditionalTests.java
+++ src/test/java/castle/comp3021/assignment/AdditionalTests.java
@@ -1,10 +1,12 @@
 package castle.comp3021.assignment;
 
-import castle.comp3021.assignment.mock.*;
+import castle.comp3021.assignment.mock.MockPiece;
+import castle.comp3021.assignment.mock.MockPlayer;
+import castle.comp3021.assignment.piece.*;
 import castle.comp3021.assignment.player.ConsolePlayer;
+import castle.comp3021.assignment.player.RandomPlayer;
 import castle.comp3021.assignment.protocol.*;
 import castle.comp3021.assignment.protocol.exception.InvalidConfigurationError;
-import castle.comp3021.assignment.util.SampleTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -48,6 +50,26 @@ public class AdditionalTests {
     }
 
     /**
+     * Test arguments passed into JesonMor.updateScore()
+     */
+    @Test
+    public void testUpdateScoreArgumentValidation() {
+        MockPlayer mockPlayer = new MockPlayer();
+        MockPiece mockPiece = new MockPiece(mockPlayer);
+        Move mockMove = new Move(new Place(0, 0), new Place(1, 0));
+        Configuration config = new Configuration(5, new Player[]{mockPlayer, new MockPlayer()});
+        config.addInitialPiece(mockPiece, 0, 0);
+        JesonMor game = new JesonMor(config);
+        int oldScore = mockPlayer.getScore();
+        game.updateScore(null, mockPiece, mockMove);
+        assertEquals(oldScore, mockPlayer.getScore());
+        game.updateScore(mockPlayer, null, mockMove);
+        assertEquals(oldScore, mockPlayer.getScore());
+        game.updateScore(mockPlayer, mockPiece, null);
+        assertEquals(oldScore, mockPlayer.getScore());
+    }
+
+    /**
      * Test arguments passed into JesonMor.getWinner()
      */
     @Test
@@ -79,6 +101,82 @@ public class AdditionalTests {
         JesonMor game = new JesonMor(config);
         winner.setNextMoves(new Move[]{capturingMove});
         assertEquals(winner, game.start());
+    }
+
+    /**
+     * Test arguments passed into JesonMor.getAvailableMoves()
+     */
+    @Test
+    public void testGetAvailableMovesArgumentValidation1() {
+        MockPlayer mockPlayer = new MockPlayer();
+        MockPiece mockPiece = new MockPiece(mockPlayer);
+        Configuration config = new Configuration(5, new Player[]{mockPlayer, new MockPlayer()});
+        config.addInitialPiece(mockPiece, 0, 0);
+        JesonMor game = new JesonMor(config);
+        Move[] emptyMoves = new Move[0];
+        assertArrayEquals(emptyMoves, game.getAvailableMoves(null));
+    }
+
+    /**
+     * Test arguments passed into Archer.getAvailableMoves()
+     */
+    @Test
+    public void testGetAvailableMovesArgumentValidation2() {
+        MockPlayer mockPlayer = new MockPlayer();
+        Piece archerPiece = new Archer(mockPlayer);
+        Configuration config = new Configuration(5, new Player[]{mockPlayer, new MockPlayer()});
+        config.addInitialPiece(archerPiece, 0, 0);
+        JesonMor game = new JesonMor(config);
+        Move[] emptyMoves = new Move[0];
+        assertArrayEquals(emptyMoves, archerPiece.getAvailableMoves(null, null));
+        assertArrayEquals(emptyMoves, archerPiece.getAvailableMoves(game, null));
+        assertArrayEquals(emptyMoves, archerPiece.getAvailableMoves(game, new Place(1, 0)));
+    }
+
+    /**
+     * Test arguments passed into Knight.getAvailableMoves()
+     */
+    @Test
+    public void testGetAvailableMovesArgumentValidation3() {
+        MockPlayer mockPlayer = new MockPlayer();
+        Piece knightPiece = new Knight(mockPlayer);
+        Configuration config = new Configuration(5, new Player[]{mockPlayer, new MockPlayer()});
+        config.addInitialPiece(knightPiece, 0, 0);
+        JesonMor game = new JesonMor(config);
+        Move[] emptyMoves = new Move[0];
+        assertArrayEquals(emptyMoves, knightPiece.getAvailableMoves(null, null));
+        assertArrayEquals(emptyMoves, knightPiece.getAvailableMoves(game, null));
+        assertArrayEquals(emptyMoves, knightPiece.getAvailableMoves(game, new Place(1, 0)));
+    }
+
+    /**
+     * Test arguments passed into ConsolePlayer.nextMove()
+     */
+    @Test
+    public void testNextMoveArgumentValidation1() {
+        ConsolePlayer userPlayer = new ConsolePlayer("UserPlayer");
+        MockPiece mockPiece = new MockPiece(userPlayer);
+        Configuration config = new Configuration(5, new Player[]{userPlayer, new MockPlayer()});
+        config.addInitialPiece(mockPiece, 0, 0);
+        JesonMor game = new JesonMor(config);
+        assertThrows(Exception.class, () -> userPlayer.nextMove(null, null));
+        assertThrows(Exception.class, () -> userPlayer.nextMove(game, null));
+        assertThrows(Exception.class, () -> userPlayer.nextMove(game, new Move[0]));
+    }
+
+    /**
+     * Test arguments passed into RandomPlayer.nextMove()
+     */
+    @Test
+    public void testNextMoveArgumentValidation2() {
+        RandomPlayer computerPlayer = new RandomPlayer("RandomPlayer");
+        MockPiece mockPiece = new MockPiece(computerPlayer);
+        Configuration config = new Configuration(5, new Player[]{computerPlayer, new MockPlayer()});
+        config.addInitialPiece(mockPiece, 0, 0);
+        JesonMor game = new JesonMor(config);
+        assertThrows(Exception.class, () -> computerPlayer.nextMove(null, null));
+        assertThrows(Exception.class, () -> computerPlayer.nextMove(game, null));
+        assertThrows(Exception.class, () -> computerPlayer.nextMove(game, new Move[0]));
     }
 
     /**
